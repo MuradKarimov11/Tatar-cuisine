943c5f6e53e8395bf052f786eb27d61c8771ad56
Murad Karimov <muradkarimov07@gmail.com>
2025-10-29 17:05:05 +0300

    created web page recipe

--- src/constants/select-options.ts
+++ src/constants/select-options.ts
@@ -18,6 +18,14 @@ export const UNIT_OPTIONS = [
     { value: 'cup', label: 'чашка' },
 ] as const;
 
+export const UNIT_ABBREVIATIONS = [
+  { value: "GRAMS", label: "г" },
+  { value: "KILOGRAMS", label: "кг" },
+  { value: "LITERS", label: "л" },
+  { value: "MILLILITERS", label: "мл" },
+  { value: "PIECES", label: "шт" }
+] as const;
+
 
 
     
